846a010ac35e354aa6e328cadc95ee309e7bf74c
Yukesh Katuwal <93563263+Ukeshgit@users.noreply.github.com>
2025-07-20 22:10:20 +0545

    small changes

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.deepPurple,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: CounterApp(),
